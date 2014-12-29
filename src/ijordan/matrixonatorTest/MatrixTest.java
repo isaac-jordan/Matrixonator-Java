28e158615b1be88d1e6eef3cbfe2546b5b2df2de
Isaac J <sheepses6@gmail.com>
2014-12-29 02:16:22 +0000

    Added first JUnit test

---
+++ src/ijordan/matrixonatorTest/MatrixTest.java
@@ -0,0 +1,20 @@
+package ijordan.matrixonatorTest;
+
+import static org.junit.Assert.assertEquals;
+import ijordan.matrixonator.model.Matrix;
+
+import org.junit.Test;
+
+public class MatrixTest {
+	
+	@Test
+    public void testMatrixCreation(){
+		double[][] data = new double[5][2];
+		
+        final Matrix testMatrix = new Matrix("Test", data);
+        assertEquals("Created matrix has inaccurate number of rows", testMatrix.getNumRows(), 5);
+        assertEquals("Created matrix has inaccurate number of columns",testMatrix.getNumCols(), 2);
+        
+    }
+
+}
