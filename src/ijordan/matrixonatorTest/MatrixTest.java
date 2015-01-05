84340fb19f005896e2aa08e31e3d093378da4f66
Isaac J <sheepses6@gmail.com>
2015-01-05 19:21:08 +0000

    Added two reduced row echelon form tests that show the function is broken currently.

--- src/ijordan/matrixonatorTest/MatrixTest.java
+++ src/ijordan/matrixonatorTest/MatrixTest.java
@@ -98,19 +98,37 @@ public class MatrixTest {
     }
 	
 	@Test
-    public void testMatrixReduce(){
-		double[][] dataFirst = {{1,0,3,3},{0,1,0,4},{0,0,0,1}};
-		double[][] dataSecond = {{6543, 7896, 9876, 156}, {76546, 765467, 4657, 573}, {34765, 28452, 9876789, 475645323234.0}};
-		
-		double[][] dataResult1 = {{1,0,3,0},{0,1,0,0},{0,0,0,1}};
-		
-        final Matrix testMatrix1 = new Matrix("Test1", dataFirst);
-        final Matrix testMatrix2 = new Matrix("Test2", dataSecond);
+    public void testMatrixReduceNormal(){
+		double[][] data = {{1,0,3,3},{0,1,0,4},{0,0,0,1}};
+		double[][] dataResult = {{1,0,3,0},{0,1,0,0},{0,0,0,1}};
+        final Matrix testMatrix = new Matrix("Test1", data);
         
         assertEquals("Matrix Reduction gives incorrect result", 
-        		dataResult1, 
-        		testMatrix1.reducedEchelonForm().getData());
-        
+        		dataResult, 
+        		testMatrix.reducedEchelonForm().getData());   
     }
+	
+	@Test
+    public void testMatrixReduceNegative(){
+		double[][] data = {{1,2,1},{-2,-3,1},{3,5,0}};
+		double[][] dataResult = {{1,0,-5},{0,1,3},{0,0,0}};
+		final Matrix testMatrix = new Matrix("Test2", data);
+		
+		assertEquals("Matrix Reduction gives incorrect result", 
+        		dataResult, 
+        		testMatrix.reducedEchelonForm().getData());
+	}
+	
+	@Test
+	public void testMatrixReduceLargeValue() {
+		double[][] data = {{1586, 7546, 85746, 57564}, {756, 374, 385, 0}, {8765, 123, 765, 234}, {5736, 4736, 27364, 5}};
+		double[][] dataResult = {{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}};
+		final Matrix testMatrix = new Matrix("Test2", data);
+		
+		assertEquals("Matrix Reduction gives incorrect result", 
+        		dataResult, 
+        		testMatrix.reducedEchelonForm().getData());
+		
+	}
 
 }
