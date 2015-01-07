a2be42db8e3d12a21f2519b9216dc18656582dc2
Isaac J <sheepses6@gmail.com>
2015-01-07 01:49:49 +0000

    Added new test for RREF, and for EROs. RREF algorithm is still broken for most tests.

--- src/ijordan/matrixonator/model/Matrix.java
+++ src/ijordan/matrixonator/model/Matrix.java
@@ -16,7 +16,7 @@ public class Matrix {
 	private final IntegerProperty numRows;
 	private final IntegerProperty numCols;
 	private final ObjectProperty<LocalDate> createdDate;
-	private final ObjectProperty<double[][]> data;
+	private ObjectProperty<double[][]> data;
 
 	/**
 	 * Default constructor. Creates an empty, unnamed matrix.
@@ -122,21 +122,51 @@ public class Matrix {
 		int i = 0;
 		int j = 0;
 		while (i < this.getNumRows() && j < this.getNumCols()) {
-			j = stepOne(localMatrix, i, j);
-			System.out.println("Solving...");
+/*			OLD CODE THAT IS BROKEN, STILL HERE FOR REFERENCE
+ * stepOne(localMatrix, i, j);
+
+			if (i == localMatrix.getNumRows() - 1) {
+				if (j == localMatrix.getNumCols() - 1) {
+					stepThree(localMatrix, i, j);
+					break;
+				} else {
+					while (j != localMatrix.getNumCols()) {
+						if (localMatrix.getData()[i][j] == 0) {
+							//Do nothing
+						} else {
+							stepTwo(localMatrix, i, j);
+						}
+						stepThree(localMatrix, i, j);
+						j += 1;
+					}
+				}
+			} else {
+				if (localMatrix.getData()[i][j] == 0) {
+					//Do nothing
+				} else {
+					stepTwo(localMatrix, i, j);
+				}
+				stepThree(localMatrix, i, j);
+			}*/
+			
+			if (stepOne(localMatrix, i, j) && j != localMatrix.getNumCols()-1) {
+				j++;
+			}
 			stepTwo(localMatrix, i, j);
-			System.out.println("Solving...");
+			
 			stepThree(localMatrix, i, j);
-			System.out.println("Solving...");
+
 			i += 1;
 			j += 1;
+			
+
 		}
-		System.out.println(Arrays.deepToString(localMatrix.getData()));
+		System.out.println("RREF: " + Arrays.deepToString(localMatrix.getData()));
 		return localMatrix;
 
 	}
 
-	private Matrix ERO1(Matrix A, int row1, int row2) {
+	public Matrix ERO1(Matrix A, int row1, int row2) {
 		// Swaps row1 and row2
 		double[] temp = A.getData()[row1];
 		A.getData()[row1] = A.getData()[row2];
@@ -144,15 +174,16 @@ public class Matrix {
 		return A;
 	}
 
-	private Matrix ERO2(Matrix A, int row, double scalar) {
+	public Matrix ERO2(Matrix A, int row, double scalar) {
 		// Multiply every element of row by scalar
-		for (double value : A.getData()[row]) {
-			value *= scalar;
+		for (int i = 0; i < A.getNumCols(); i++) {
+			A.getData()[row][i] *= scalar;
 		}
 		return A;
 	}
 
-	private Matrix ERO3(Matrix A, int row1, int row2, double scalar) {
+	public Matrix ERO3(Matrix A, int row1, int row2, double scalar) {
+		//row1 = row1 + scalar*row2
 		for (int i = 0; i < A.getNumCols(); i++) {
 			A.getData()[row1][i] += scalar * A.getData()[row2][i];
 		}
@@ -164,7 +195,7 @@ public class Matrix {
 	// i+1,j i+1,j+1 i+1,j+2
 	// i+2,j i+2,j+2 i+2,j+2 etc
 
-	private int stepOne(Matrix A, int i, int j) {
+	public boolean stepOne(Matrix A, int i, int j) {
 		// If A[i][j] = 0 swap the ith row with some other row (A[i+b]) below to
 		// make A[i][j] not 0.
 		// This A[i][j], non-zero entry is called a pivot.
@@ -179,19 +210,21 @@ public class Matrix {
 				A = ERO1(A, i, (i + b));
 			}
 		}
+		
 		if (A.getData()[i][j] == 0) {
-			j += 1;
-			stepOne(A, i, j);
+			return true;
+		} else {
+			return false;
 		}
-		return j;
 	}
 
-	private void stepTwo(Matrix A, int i, int j) {
+	public void stepTwo(Matrix A, int i, int j) {
 		// Divide the ith row by A[i][j] to make the pivot entry = 1
 		A = ERO2(A, i, (1 / A.getData()[i][j]));
+		
 	}
 
-	private void stepThree(Matrix A, int i, int j) {
+	public void stepThree(Matrix A, int i, int j) {
 		// Eliminate all other entries in the
 		// jth column by subtracting suitable multiples of the
 		// ith row from the other rows
--- src/ijordan/matrixonatorTest/MatrixTest.java
+++ src/ijordan/matrixonatorTest/MatrixTest.java
@@ -109,6 +109,26 @@ public class MatrixTest {
 		assertEquals("Matrix Reduction gives incorrect result", dataResult, testMatrix
 				.reducedEchelonForm().getData());
 	}
+	
+	@Test
+	public void testMatrixReduceNormal2() {
+		double[][] data = { { 15, 2, 3 }, { 0, 2, 3 } };
+		double[][] dataResult = { { 1, 0, 0 }, { 0, 1, 2 } };
+		final Matrix testMatrix = new Matrix("Test1", data);
+
+		assertEquals("Matrix Reduction gives incorrect result", dataResult, testMatrix
+				.reducedEchelonForm().getData());
+	}
+	
+	@Test
+	public void testMatrixReduceManyRows() {
+		double[][] data = {{1, 2, 3, 4}, {-1, -2, 0, 1}, {5, 6, 7, 8}, {1, 3, 2, 4}, {0, 1, 2, 3}, {5, 6, 7, 8}};
+		double[][] dataResult = {{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}, {0, 0, 0, 0}, {0, 0, 0, 0}};
+		final Matrix testMatrix = new Matrix("Test1", data);
+
+		assertEquals("Matrix Reduction gives incorrect result", dataResult, testMatrix
+				.reducedEchelonForm().getData());
+	}
 
 	@Test
 	public void testMatrixReduceNegative() {
@@ -131,5 +151,61 @@ public class MatrixTest {
 				.reducedEchelonForm().getData());
 
 	}
+	
+	@Test
+	public void testERO1() {
+		double[][] data = { { 1, 2, 1 }, { -2, -3, 1 }, { 3, 5, 0 } };
+		double[][] dataResult = { { 1, 2, 1 }, { 3, 5, 0 }, { -2, -3, 1 } };
+		final Matrix testMatrix = new Matrix("Test2", data);
+
+		assertEquals("Matrix ERO1 gives incorrect result", dataResult, testMatrix
+				.ERO1(testMatrix, 2, 1).getData());
+	}
+	
+	@Test
+	public void testERO2Negative() {
+		double[][] data = { { 1, 2, 1 }, { -2, -3, 1 }, { 3, 5, 0 } };
+		double[][] dataResult = { { 1, 2, 1 }, { 2, 3, -1 }, { 3, 5, 0 } };
+		final Matrix testMatrix = new Matrix("Test2", data);
+
+		assertEquals("Matrix ERO2 gives incorrect result", dataResult, testMatrix
+				.ERO2(testMatrix, 1, -1).getData());
+	}
+	
+	@Test
+	public void testERO3Negative() {
+		double[][] data = { { 1, 2, 1 }, { -2, -3, 1 }, { 3, 5, 0 } };
+		double[][] dataResult = { { 1, 2, 1 }, { -8, -13, 1 }, { 3, 5, 0 } };
+		final Matrix testMatrix = new Matrix("Test2", data);
+
+		assertEquals("Matrix ERO3 gives incorrect result", dataResult, testMatrix
+				.ERO3(testMatrix, 1, 2, -2).getData());
+	}
+	
+	@Test
+	public void testStepOne() {
+		double[][] data = { { 1, 2, 1 }, { -2, 0, 1 }, { 3, 5, 0 } };
+		double[][] dataResult = { { 1, 2, 1 }, { 3, 5, 0 }, { -2, 0, 1 } };
+		final Matrix testMatrix = new Matrix("Test2", data);
+		
+		testMatrix.stepOne(testMatrix, 1, 1);
+
+		assertEquals("Matrix stepOne gives incorrect result", dataResult, testMatrix.getData());
+	}
+	
+	@Test
+	public void testStepOne2() {
+		double[][] data = {{1.0, 0.0, -5.0}, {0.0, 1.0, 3.0}, {0.0, 0.0, 0.0}};
+		double[][] dataResult = {{1.0, 0.0, -5.0}, {0.0, 1.0, 3.0}, {0.0, 0.0, 0.0}};
+		final Matrix testMatrix = new Matrix("Test2", data);
+		
+		testMatrix.stepOne(testMatrix, 2, 2);
+
+		assertEquals("Matrix stepOne gives incorrect result", dataResult, testMatrix.getData());
+	}
+	
+	
+	
+	
 
 }
